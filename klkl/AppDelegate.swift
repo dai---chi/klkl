6a5f9dfea82ecafb5e4a5e8dd8859493a1c76fb1
daichi <daipanchi@gmail.com>
2015-08-16 16:13:05 +0900

    Create shortcut to press double command key

--- klkl/AppDelegate.swift
+++ klkl/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  klkl
+//  glgl
 //
-//  Created by dch on 8/16/15.
+//  Created by dch on 8/15/15.
 //  Copyright (c) 2015 co.l0o0l. All rights reserved.
 //
 
@@ -10,18 +10,30 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    let theWorkspace : NSWorkspace = NSWorkspace.sharedWorkspace()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+                NSEvent.addGlobalMonitorForEventsMatchingMask(NSEventMask.FlagsChangedMask, handler: handlerEvent)
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    var prevTimeInterval = 0.0
+    func handlerEvent(aEvent: (NSEvent!)) -> Void {
+        if aEvent.modifierFlags.rawValue & NSEventModifierFlags.CommandKeyMask.rawValue > 0 {
+            println("command")
+            if (aEvent.timestamp - prevTimeInterval < 0.2) {
+                println( "double Command was pressed" );
+                if theWorkspace.launchApplication("Google Chrome"){
+                    NSLog("OK")
+                } else {
+                    NSLog("NO")
+                }
+            }
+            prevTimeInterval = aEvent.timestamp;
+        }
     }
-
-
+    
+//    func applicationWillTerminate(aNotification: NSNotification) {
+//        Insert code here to tear down your application
+//    }
 }
-
